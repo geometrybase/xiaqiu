e9d1fc7940236438e850e767f57ae69dbda2329c
chenjilin <chenjilin@chenjilins-MacBook-Pro.local>
2022-10-21 00:44:45 +0800

    04-add-bilinear

--- src/02-Add-Color.js
+++ src/02-Add-Color.js
@@ -141,31 +141,28 @@ void main() {
   vec3 col5 = rgb2hsl(color5); 
 
   vec3 col = rgb2hsl(texture2D(t, uv.xy).xyz);
-  float l = sqrt(col.z);
+  vec3 origin_col = col;
+  float l = (col.z - 0.5)*1.0 + 0.5;
   vec3 start = vec3(0.0, 0.0, 0.0);
   vec3 end = vec3(1.0, 1.0, 1.0);
-  if (l < 0.34) {
+  if (l < 0.44) {
     col.xyz = mix(start, col1.xyz, l/0.34*l/0.34);
-    // col.xyz = vec3(0.0, 1.0, 0.5);
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
   }else if (l < 0.42) {
     col.xyz = mix(col1.xyz, col2.xyz, (l-0.34)/0.08);
-    col.z = l;
-    // col.xyz = vec3(0.2, 1.0, 0.5);
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
   }else if (l < 0.50) {
     col.xyz = mix(col2.xyz, col3.xyz, (l-0.42)/0.08);
-    col.z = l;
-    // col.xyz = vec3(0.4, 1.0, 0.5);
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
   }else if (l < 0.58) {
     col.xyz = mix(col3.xyz, col4.xyz, (l-0.50)/0.08);
-    col.z = l;
-    // col.xyz = vec3(0.6, 1.0, 0.5);
-  }else if (l < 0.66){
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
+  }else if (l < 0.76){
     col.xyz = mix(col4.xyz, col5.xyz, (l-0.58)/0.08);
-    col.z = l;
-    // col.xyz = vec3(0.8, 1.0, 0.5);
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
   }else {
     col.xyz = mix(col5.xyz, end, (l-0.66)/0.34);
-    // col.xyz = vec3(1.0, 1.0, 0.5);
+    col.yz =  mix(origin_col.yz, col.yz, 0.5);
   }
   col = hsl2rgb(col);
   gl_FragColor = vec4(col, 1.0);
